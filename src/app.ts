4d3db1aa58a503d2ed32bf134a813b6d45014d5b
KeKs0r <marc.hoeffl@laserfocus.io>
2024-12-07 06:06:42 +0100

    ♻️: Refactor app.ts to improve commit generation flow
    Refactored the application flow to include a spinner for diff retrieval and handle commit confirmation cancellation. Removed unused imports and added utility for string length conversion.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
-import { confirm, spinner, intro, outro, log } from "@clack/prompts";
-import { Chalk } from "chalk";
+import { confirm, spinner, intro, outro, isCancel } from "@clack/prompts";
+
 import { generateCommits } from "./generate-commits";
 import { commitFiles, getDiff, getStatus } from "./git";
 import { renderCommits } from "./tui";
-
-const color = new Chalk();
+import { stringLengthToBytes } from "./util";
 
 export async function runApp() {
   intro("Comitting your changes");
@@ -13,17 +12,23 @@ export async function runApp() {
     outro("Everything is clean, nothing to commmit");
     return;
   }
+  const dSpinner = spinner();
+  dSpinner.start("Getting diff...");
   const diff = await getDiff();
+  dSpinner.stop(`Got diff (${stringLengthToBytes(diff.length)})`);
+
   const s = spinner();
   s.start("Generating commits...");
   const commits = await generateCommits({ diff });
   s.stop("Generated commits");
+
   renderCommits(commits);
   const shouldCommit = await confirm({
     message: "Do you want to commit?",
   });
-  console.log("ShouldCommit", shouldCommit);
-  if (shouldCommit) {
+  if (isCancel(shouldCommit)) {
+    outro("Cancelled");
+  } else if (shouldCommit) {
     await commitFiles(commits);
   } else {
     outro("not done anything");
