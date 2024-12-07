0f849e9fce4f4a896b22b148e7a8e5595ef0054e
Marc Höffl <marc.hoeffl@laserfocus.io>
2024-12-07 13:07:17 +0800

    add clack ui (#1)
    * 📝: Update README with project name
    
    * ⬆️: Add @clack/prompts dependency
    
    * ✨: Enhance runApp with user confirmation and spinner
    
    * ✨: Log usage in generateCommits function
    
    * 🔧: Update bun.lockb file
    
    * ⬆️: Add 'chalk' dependency to package.json
    
    * ✨: Enhance logging in runApp with new prompts and chalk
    
    * ♻️: Remove unnecessary console.log in generateCommits
    
    * ♻️: Refactor logging to use Clack prompts
    
    * ⬆️: Replace chalk with picocolors for color handling
    
    * 💡: Add debug log for commit confirmation
    
    * ✨: Enhance commit rendering with picocolors
    
    * ⬆️: Update bun.lockb file
    
    * ♻️: Refactor app.ts to improve commit generation flow
    
    Refactored the application flow to include a spinner for diff retrieval and handle commit confirmation cancellation. Removed unused imports and added utility for string length conversion.

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,36 @@
+import { confirm, spinner, intro, outro, isCancel } from "@clack/prompts";
+
 import { generateCommits } from "./generate-commits";
 import { commitFiles, getDiff, getStatus } from "./git";
 import { renderCommits } from "./tui";
+import { stringLengthToBytes } from "./util";
 
 export async function runApp() {
+  intro("Comitting your changes");
   const status = await getStatus();
   if (status.isClean()) {
-    console.log("Everything is clean, nothing to commmit");
-    console.log(status);
+    outro("Everything is clean, nothing to commmit");
     return;
   }
+  const dSpinner = spinner();
+  dSpinner.start("Getting diff...");
   const diff = await getDiff();
+  dSpinner.stop(`Got diff (${stringLengthToBytes(diff.length)})`);
+
+  const s = spinner();
+  s.start("Generating commits...");
   const commits = await generateCommits({ diff });
+  s.stop("Generated commits");
+
   renderCommits(commits);
-  await commitFiles(commits);
+  const shouldCommit = await confirm({
+    message: "Do you want to commit?",
+  });
+  if (isCancel(shouldCommit)) {
+    outro("Cancelled");
+  } else if (shouldCommit) {
+    await commitFiles(commits);
+  } else {
+    outro("not done anything");
+  }
 }
