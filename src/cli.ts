eb6a1d4fbb846ff27ef9edfc33cd072e96f1fcd5
KeKs0r <marc.hoeffl@laserfocus.io>
2024-12-12 21:10:46 +0100

    🔥 Remove unused argument parsing code from CLI
    - Removed commented-out code related to argument parsing in CLI.
    - Simplified the run function in cli.ts.

--- src/cli.ts
+++ src/cli.ts
@@ -2,20 +2,6 @@ import { parseArgs } from "util";
 import { runApp } from "./app";
 
 export async function run() {
-  const {} = parseArgs({
-    args: Bun.argv,
-    // options: {
-    //   flag1: {
-    //     type: "boolean",
-    //   },
-    //   flag2: {
-    //     type: "string",
-    //   },
-    // },
-    strict: true,
-    allowPositionals: true,
-  });
-
   // console.log(values);
   // console.log(positionals);
 
